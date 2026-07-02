328c6720691160d67ea70d120f822449b94424cf
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-02 20:43:07 +0530

    Added the fomrula and it's explaination

---
+++ main.py
@@ -0,0 +1,7 @@
+# The mode formula is |
+#  mode = l + (f1 - f0 / 2f1 - f0 - f2) * h
+
+# Explaination :
+# \(l\) = Lower limit of the modal class (the class interval with the highest frequency).\(h\) = Size of the class interval (assuming all class sizes are equal).\(f_{1}\) = Frequency of the modal class.\(f_{0}\) = Frequency of the class preceding (before) the modal class.\(f_{2}\) = Frequency of the class succeeding (after) the modal class.
+
+
