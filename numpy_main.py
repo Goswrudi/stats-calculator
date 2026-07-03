6a7836396f071a3decdffa4e25bb48e800a2ee1f
rudransh goswami <rudranshgoswami415@gmail.com>
2026-07-03 22:33:12 +0530

    Added the funcion for mode , median in numpy

--- numpy_main.py
+++ numpy_main.py
@@ -1,13 +1,21 @@
 # doing the same thing with numpy functions 
 
 import numpy as np 
+import random
 
-array = np.array([1 , 2 , 3 , 4 , 5 ]) 
 
-print(np.mean(array))
+def stats_numpy():
+  array = np.array([1 , 2 , 3 , 4 , 5 ]) 
+  print(np.mean(array))
+  print(np.median(array))
 
-print(np.median(array))
+stats_numpy()
+
 
 # Can we use the data as random? 
 
 # since we do not have any user input we can definatly use the random library function...
+
+
+array1 = random.randint(0 , 10)
+ 
